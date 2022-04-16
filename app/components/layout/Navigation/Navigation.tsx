70e18e1802c396698e620681235b3084da7b7479
BekhaIrga <irgashevbehzod@gmail.com>
2022-04-16 16:58:26 +0500

    Initial commit to github

--- app/components/layout/Navigation/Navigation.tsx
+++ app/components/layout/Navigation/Navigation.tsx
@@ -3,7 +3,9 @@ import { FC } from 'react'
 import styles from './Navigation.module.scss'
 
 const Navigation: FC = () => {
-	return <div>Navigation</div>
+	return <div className={styles.navigation}>
+		<Logo />
+	</div>
 }
 
 export default Navigation
